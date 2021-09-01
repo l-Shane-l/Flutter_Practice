7246ec9a2bfa45a68c7996069200ae917cefe5ab
lShanel <shane.hamilton3@mail.dcu.ie>
2021-09-01 06:01:20 +0100

    basic question answer

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
+    var question = ['Q1', 'Q2', 'Q3'];
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text('My App'),),
-      body: Text('Default Text'),
+      body: Column(children: <Widget>[
+        Text('question 1'),
+        RaisedButton(child: Text('Answer 1'), onPressed: null),
+        RaisedButton(child: Text('Answer 2'), onPressed: null),
+        RaisedButton(child: Text('Answer 3'), onPressed: null),
+
+      ],),
     ),);
   }
 }
